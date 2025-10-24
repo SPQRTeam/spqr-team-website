64b228a8faa43051ee3f6cdd07afa4f6531207a5
Valerio Spagnoli <spagnoli.valerio.00@gmail.com>
2025-10-24 16:44:19 +0200

    some events

--- src/pages/events.vue
+++ src/pages/events.vue
@@ -6,4 +6,154 @@
 </template>
 
 <script setup>
+
+const events = [
+
+    //* --- 2025 --- *//
+    {
+        name: 'Maker Faire 2025 - Rome, Italy',
+        year: 2025,
+        description: 'The SPQR team participated in friendly tournament during the Maker Faire in Rome (Italy), 17-19 October 2025, with HTWK (Leipzig University), and Tech United (Eindhoven University). This was the first eurpean tournament with Booster Robotics robots!',
+        cover: '',
+        links: [
+            {
+                site: 'https://makerfairerome.eu/it/',
+                video: '',
+                results: '',
+            },
+        ],
+    },
+    
+    {
+        name: 'RoboCup 2025 - Salvador de Bahia, Brasil',
+        year: 2025,
+        description: 'The SPQR Team competed in the Champions Cup, achieving third place in the seeding round and finishing fourth overall. Additionally, the team earned third place in the KICKin Rollin Technical Challenge!',
+        cover: '',
+        links: [
+            {
+                site: 'https://2025.robocup.org/',
+                video: '',
+                results: 'https://spl.robocup.org/results-2025/',
+            },
+        ],
+    },
+
+    //* --- 2024 --- *//
+    {
+        name: 'Maker Faire 2024 - Rome, Italy',
+        year: 2024,
+        description: 'The SPQR team participated in friendly tournament during the Maker Faire in Rome (Italy), 25-27 October 2024, with HTWK (Leipzig University).',
+        cover: '',
+        links: [
+            {
+                site: 'https://makerfairerome.eu/it/',
+                video: '',
+                results: '',
+            },
+        ],
+    },
+    
+    {
+        name: 'RoboCup 2024 - Eindhoven, Netherlands',
+        year: 2024,
+        description: 'The SPQR Team competed in the Champions Cup, achieving third place in the seeding round and finishing fifth overall. Additionally, the team earned third place in the Shared Autonomy Challenge!',
+        cover: '',
+        links: [
+            {
+                site: 'https://2024.robocup.org/',
+                video: 'https://www.youtube.com/watch?v=lAl1qg1B2WM',
+                results: 'https://spl.robocup.org/results-2024/',
+            },
+        ],
+    },
+
+    //* --- 2023 --- *//
+    {
+        name: 'Maker Faire 2023 - Rome, Italy',
+        year: 2023,
+        description: 'The SPQR team participated in a triangular friendly tournament during the Maker Faire in Rome (Italy), 20-22 October 2023, with NaoDevis (Dortmunt Univesity) and Hulks (Hamburg Univesity).',
+        cover: '',
+        links: [
+            {
+                site: 'https://makerfairerome.eu/it/',
+                video: '',
+                results: '',
+            },
+        ],
+    },
+
+    {
+        name: 'RoboCup 2023 - Bordeaux, France',
+        year: 2023,
+        description: 'The RoboCup 2023 took place in Bourdeax (France) from 4 to 10 July 2023. The SPQR team competed in the Champions Cup winning the seventh place and the quarter-finals. At the symposium, held in University of Bordeaux, the SPQR team won the Best Paper Award.',
+        cover: '',
+        links: [
+            {
+                site: 'https://2023.robocup.org/en/home/',
+                video: 'https://www.youtube.com/watch?v=DXEUjxQSiG0',
+                results: 'https://spl.robocup.org/results-2023/',
+            },
+        ],
+    },
+
+    //* --- 2022 --- *//
+    {
+        name: 'Maker Faire 2022 - Rome, Italy',
+        year: 2022,
+        description: 'The SPQR team participated in a friendly series of matches against Nao Devils team during the Maker Faire in Rome (Italy), 7-9 October 2022.',
+        cover: '',
+        links: [
+            {
+                site: 'https://makerfairerome.eu/it/',
+                video: 'https://www.youtube.com/watch?v=HJP8uQ0UoJ8',
+                results: '',
+            },
+        ],
+    },
+
+    {
+        name: 'RoboCup 2022 - Bangkok, Thailand (Online)',
+        year: 2022,
+        description: 'The RoboCup 2022 took place in Bangkok (Thailand) from 13 to 17 July 2022. The SPQR team competed in the Standard Platform League achieving  the eleventh place.',
+        cover: '',
+        links: [
+            {
+                site: 'https://2022.robocup.org/',
+                video: 'https://www.youtube.com/watch?v=s92LMa1njy4',
+                results: 'https://spl.robocup.org/results-2022/',
+            },
+        ],
+    },
+
+    //* --- 2019 --- *//
+    {
+        name: 'Maker Faire 2019 - Rome, Italy',
+        year: 2019,
+        description: 'The SPQR team participated in a triangular friendly tournament during the Maker Faire in Rome (Italy), 18-20 October 2019.',
+        cover: '',
+        links: [
+            {
+                site: 'https://makerfairerome.eu/it/',
+                video: '',
+                results: '',
+            },
+        ],
+    },
+
+    {
+        name: 'RoboCup 2022 - Bangkok, Thailand (Online)',
+        year: 2022,
+        description: 'The RoboCup 2019 was held in Sydney (Australia), 2-8 July 2019. SPQR Team reconfirmed itself in the Champions Cup including the best 12 teams in the world.',
+        cover: '',
+        links: [
+            {
+                site: 'https://2019.robocup.org/',
+                video: '',
+                results: 'https://spl.robocup.org/results-2019/',
+            },
+        ],
+    },
+]
+
+
 </script>
