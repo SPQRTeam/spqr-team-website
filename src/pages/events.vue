e6e9a27bd9e16f3b54f16282d109e079ba418ddb
Valerio Spagnoli <spagnoli.valerio.00@gmail.com>
2025-10-24 19:27:06 +0200

    evenrs and papers

--- src/pages/events.vue
+++ src/pages/events.vue
@@ -13,6 +13,8 @@ const events = [
     {
         name: 'Maker Faire 2025 - Rome, Italy',
         year: 2025,
+        month: 'October',
+        days: '17-19',
         description: 'The SPQR team participated in friendly tournament during the Maker Faire in Rome (Italy), 17-19 October 2025, with HTWK (Leipzig University), and Tech United (Eindhoven University). This was the first eurpean tournament with Booster Robotics robots!',
         cover: '',
         links: [
@@ -23,11 +25,45 @@ const events = [
             },
         ],
     },
-    
+
+    {
+        name: 'World Humanoid Robot Games - Beijing, China',
+        year: 2025,
+        month: 'August',
+        days: '',
+        description: 'The SPQR Team competed in the first World Humanoid Robot Games held in Beijing in August 2025.',
+        cover: '',
+        links: [
+            {
+                site: '',
+                video: '',
+                results: '',
+            },
+        ],
+    },
+
     {
         name: 'RoboCup 2025 - Salvador de Bahia, Brasil',
         year: 2025,
+        month: 'July',
+        days: '',
         description: 'The SPQR Team competed in the Champions Cup, achieving third place in the seeding round and finishing fourth overall. Additionally, the team earned third place in the KICKin Rollin Technical Challenge!',
+        cover: '',
+        links: [
+            {
+                site: 'https://2025.robocup.org/',
+                video: '',
+                results: 'https://spl.robocup.org/results-2025/',
+            },
+        ],
+    },
+
+    {
+        name: 'RoboCup German Open 2025 - Nuremberg, Germany',
+        year: 2025,
+        month: 'March',
+        days: '10-14',
+        description: 'The SPQR Team competed in the RoboCup German Open 2025, achieving third place!',
         cover: '',
         links: [
             {
@@ -42,6 +78,8 @@ const events = [
     {
         name: 'Maker Faire 2024 - Rome, Italy',
         year: 2024,
+        month: 'October',
+        days: '25-27',
         description: 'The SPQR team participated in friendly tournament during the Maker Faire in Rome (Italy), 25-27 October 2024, with HTWK (Leipzig University).',
         cover: '',
         links: [
@@ -56,6 +94,8 @@ const events = [
     {
         name: 'RoboCup 2024 - Eindhoven, Netherlands',
         year: 2024,
+        month: 'July',
+        days: '',
         description: 'The SPQR Team competed in the Champions Cup, achieving third place in the seeding round and finishing fifth overall. Additionally, the team earned third place in the Shared Autonomy Challenge!',
         cover: '',
         links: [
@@ -71,6 +111,8 @@ const events = [
     {
         name: 'Maker Faire 2023 - Rome, Italy',
         year: 2023,
+        month: 'October',
+        days: '20-22', 
         description: 'The SPQR team participated in a triangular friendly tournament during the Maker Faire in Rome (Italy), 20-22 October 2023, with NaoDevis (Dortmunt Univesity) and Hulks (Hamburg Univesity).',
         cover: '',
         links: [
@@ -85,6 +127,8 @@ const events = [
     {
         name: 'RoboCup 2023 - Bordeaux, France',
         year: 2023,
+        month: 'July',
+        days: '',
         description: 'The RoboCup 2023 took place in Bourdeax (France) from 4 to 10 July 2023. The SPQR team competed in the Champions Cup winning the seventh place and the quarter-finals. At the symposium, held in University of Bordeaux, the SPQR team won the Best Paper Award.',
         cover: '',
         links: [
@@ -100,6 +144,8 @@ const events = [
     {
         name: 'Maker Faire 2022 - Rome, Italy',
         year: 2022,
+        month: 'October',
+        days: '7-9',
         description: 'The SPQR team participated in a friendly series of matches against Nao Devils team during the Maker Faire in Rome (Italy), 7-9 October 2022.',
         cover: '',
         links: [
@@ -114,6 +160,8 @@ const events = [
     {
         name: 'RoboCup 2022 - Bangkok, Thailand (Online)',
         year: 2022,
+        month: 'July',
+        days: '13-17',
         description: 'The RoboCup 2022 took place in Bangkok (Thailand) from 13 to 17 July 2022. The SPQR team competed in the Standard Platform League achieving  the eleventh place.',
         cover: '',
         links: [
@@ -129,6 +177,8 @@ const events = [
     {
         name: 'Maker Faire 2019 - Rome, Italy',
         year: 2019,
+        month: 'October',
+        days: '18-20',
         description: 'The SPQR team participated in a triangular friendly tournament during the Maker Faire in Rome (Italy), 18-20 October 2019.',
         cover: '',
         links: [
@@ -141,8 +191,10 @@ const events = [
     },
 
     {
-        name: 'RoboCup 2022 - Bangkok, Thailand (Online)',
-        year: 2022,
+        name: 'RoboCup 2019 - Sydney, Australia',
+        year: 2019,
+        month: 'July',
+        days: '2-8',
         description: 'The RoboCup 2019 was held in Sydney (Australia), 2-8 July 2019. SPQR Team reconfirmed itself in the Champions Cup including the best 12 teams in the world.',
         cover: '',
         links: [
@@ -150,6 +202,57 @@ const events = [
                 site: 'https://2019.robocup.org/',
                 video: '',
                 results: 'https://spl.robocup.org/results-2019/',
+            },
+        ],
+    },
+
+    //* --- 2018 ---*//
+    {
+        name: 'RoboCup 2018 - Montereal, Canada',
+        year: 2018,
+        month: 'June',
+        days: '18-22',
+        description: 'The RoboCup 2018 was held in Montreal (Canada), 18-22 June 2018. SPQR Team achieved the semifinals in the penalty kick competition and entered the Champions Cup including the best 12 teams in the world.',
+        cover: '',
+        links: [
+            {
+                site: 'https://2018.robocup.org/',
+                video: '',
+                results: 'https://spl.robocup.org/results-2018/',
+            },
+        ],
+    },
+
+    //* --- 2017 ---*//
+    {
+        name: 'RoboCup German Open 2017 - Magdeburg, Germany',
+        year: 2017,
+        month: 'March',
+        days: '5-7',
+        description: 'The RoboCup German Open 2017 was held in Magdeburg (Germany), 5-7 May 2017. SPQR Team achieved the 2nd Place in the Challenge Shield.',
+        cover: '',
+        links: [
+            {
+                site: 'https://robocup.de/',
+                video: '',
+                results: 'https://spl.robocup.org/results-2017/',
+            },
+        ],
+    },
+
+    //* --- 2016 ---*//
+    {
+        name: 'RoboCup 2016 - Leipzig, Germany',
+        year: 2016,
+        month: 'July',
+        days: '1-5',
+        description: 'The RoboCup 2016 was held in Leipzig (Germany), 1-5 July 2016.',
+        cover: '',
+        links: [
+            {
+                site: 'https://2016.robocup.org/web/index-2.html',
+                video: '',
+                results: 'https://spl.robocup.org/history/results-2016/',
             },
         ],
     },
--- src/pages/research.vue
+++ src/pages/research.vue
@@ -6,4 +6,71 @@
 </template>
 
 <script setup>
+
+const papers = [
+
+    {
+        title: 'LLCoach: Generating Robot Soccer Behaviors using Multi-Role Large Language Models',
+        year: 2024,
+        authors: 'M. Brienza, E. Musumeci, V. Suriani, D. Affinita, A. Pennisi, D. Nardi, D. D. Bloisi',
+        abstract: 'The deployment of robots into human scenarios necessitates advanced planning strategies, particularly when we ask robots to operate in dynamic, unstructured environments. RoboCup offers the chance to deploy robots in one of those scenarios, a human-shaped game represented by a soccer match. In such scenarios, robots must operate using predefined behaviors that can fail in unpredictable conditions. This paper introduces a novel application of Large Language Models (LLMs) to address the challenge of generating actionable plans in such settings, specifically within the context of the RoboCup Standard Platform League (SPL) competitions where robots are required to autonomously execute soccer strategies that emerge from the interactions of individual agents. In particular, we propose a multi-role approach leveraging the capabilities of LLMs to generate and refine plans for a robotic soccer team. The potential of the proposed method is demonstrated through an experimental evaluation, which has been carried out by simulating multiple matches where robots with the AI-generated plans play against robots running human built code.',
+        cover: '',
+        extras: '',
+        links: [
+            {
+                link: 'https://www.researchgate.net/publication/381736894_LLCoach_Generating_Robot_Soccer_Plans_using_Multi-Role_Large_Language_Models',
+                site: 'https://sites.google.com/diag.uniroma1.it/llcoach/',
+            },
+        ],
+    },
+
+
+    {
+        title: 'Structural Pruning for Multi-Object Detection on NAO Robots',
+        year: 2023,
+        authors: 'G. Specchi, V. Suriani, M. Brienza, F. Laus, F. Maiorana, A. Pennisi, D. Nardi, D. D. Bloisi',
+        abstract: 'In this paper, we propose a real-time multi-class detection system for the NAO V6 robot in the context of RoboCup SPL (Stan- dard Platform League) using state-of-the-art structural pruning tech- niques on neural networks derived from YOLOv7-tiny. Our approach combines structural pruning and fine-tuning, to obtain a pruned network that maintains high accuracy while reducing the number of parameters and the computational complexity of the network. The system is capa- ble of detecting various objects, including the ball, goalposts, and other robots, using the cameras of the robot. The goal has been to guarantee high speed and accuracy trade-offs suitable for the limited computa- tional resources of the NAO robot. Moreover, we demonstrate that our system can run in real-time on the NAO robot with a frame rate of 32 frames per second on 224 ×224 input images, which is sufficient for soccer competitions. Our results show that our pruned networks achieve comparable accuracy to the original network while significantly reducing the computational complexity and memory requirements. We release our annotated dataset, which consists of over 4000 images of various objects in the RoboCup SPL soccer field.',
+        cover: '',
+        extras: '',
+        links: [
+            {
+                link: 'https://www.researchgate.net/publication/375380980_Structural_Pruning_for_Real-Time_Multi-Object_Detection_on_NAO_Robots',
+                site: 'https://sites.google.com/diag.uniroma1.it/spqr-multi-object-ssd-pruning',
+            },
+        ],
+    },
+
+    {
+        title: 'Play Everywhere: A Temporal Logic based Game Environment Independent Approach for Playing',
+        year: 2023,
+        authors: 'V.Suriani, E. Musumeci, D. Nardi, D. D. Bloisi',
+        abstract: 'Robots playing soccer often rely on hard-coded behaviors that struggle to generalize when the game environment change. In this paper, we propose a temporal logic based approach that allows robots’ behaviors and goals to adapt to the semantics of the environment. In particular, we present a hierarchical representation of soccer in which the robot selects the level of operation based on the perceived semantic characteristics of the environment, thus modifying dynamically the set of rules and goals to apply. The proposed approach enables the robot to op- erate in unstructured environments, just as it happens when humans go from soccer played on an official field to soccer played on a street. Three different use cases set in different scenarios are presented to demonstrate the effectiveness of the proposed approach.',
+        cover: '',
+        extras: 'RoboCup Symposium Best Paper Award 2023',
+        links: [
+            {
+                link: 'https://arxiv.org/abs/2405.12628',
+                site: 'https://sites.google.com/diag.uniroma1.it/play-everywhere',
+            },
+        ],
+    },
+
+    {
+        title: 'MARIO: Modular and Extensible Architecture for Computing Visual Statistics in RoboCup SPL',
+        year: 2022,
+        authors: 'D. D. Bloisi, A. Pennisi, C. Zampino, F. Biancospino, F. Laus, G. Di Stefano, M. Brienza, R. Romano',
+        abstract: 'This technical report describes a modular and extensible architecture for computing visual statistics in RoboCup SPL (MARIO), presented dur- ing the SPL Open Research Challenge at RoboCup 2022, held in Bangkok (Thailand). MARIO is an open-source, ready-to-use software application whose final goal is to contribute to the growth of the RoboCup SPL com- munity. MARIO comes with a GUI that integrates multiple machine learning and computer vision based functions, including automatic cam- era calibration, background subtraction, homography computation, player + ball tracking and localization, NAO robot pose estimation and fall de- tection. MARIO has been ranked no. 1 in the Open Research Challenge.',
+        cover: '',
+        extras: '',
+        links: [
+            {
+                link: 'https://arxiv.org/abs/2209.09987',
+                site: 'https://sites.google.com/unibas.it/wolves/robocup/robocup-2022/mario',
+            },
+        ],
+    },
+]
+
+
+
 </script>
