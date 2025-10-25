b85e2636756ff7197b69ffb6a0283a319225f407
Valerio Spagnoli <spagnoli.valerio.00@gmail.com>
2025-10-26 00:03:17 +0200

    git action

--- vite.config.mjs
+++ vite.config.mjs
@@ -14,6 +14,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/spqr-team-website/' : '/',
   plugins: [
     VueRouter(),
     Layouts(),
