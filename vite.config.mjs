276ba87ee881774360fd6df630fca0ff41371049
Valerio Spagnoli <spagnoli.valerio.00@gmail.com>
2025-11-11 10:26:03 +0100

    change url

--- vite.config.mjs
+++ vite.config.mjs
@@ -14,7 +14,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/spqr-team-website/',
+  base: '/',
   plugins: [
     VueRouter(),
     Layouts(),
