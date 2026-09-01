54876755c9b61793a1aafa2cda6c4ab9eff8f35e
Valerio Spagnoli <spagnoli.valerio.00@gmail.com>
2026-09-01 13:12:17 +0200

    minor

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -25,7 +25,24 @@
     <template v-slot:append>
         <template v-if="!mobile">
             <v-btn to="/team">Team</v-btn>
-            <v-btn to="/events">Events</v-btn>
+            <v-menu open-on-hover>
+                <template v-slot:activator="{ props }">
+                    <v-btn
+                        v-bind="props"
+                        :active="eventsActive"
+                        append-icon="mdi-menu-down"
+                    >Events</v-btn>
+                </template>
+                <v-list bg-color="#822433">
+                    <v-list-item
+                        v-for="item in eventsItems"
+                        :key="item.path"
+                        :title="item.title"
+                        :to="item.path"
+                        link
+                    ></v-list-item>
+                </v-list>
+            </v-menu>
             <v-btn to="/press">Press</v-btn>
             <v-btn to="/media">Media</v-btn>
             <v-btn to="/publications">Publications</v-btn>
@@ -44,27 +61,50 @@
         temporary
     >
         <v-list>
-            <v-list-item 
-                v-for="item in menuItems" 
-                :key="item.title"
-                :title="item.title"
-                :to="item.path"
-                link
-            ></v-list-item>
+            <template v-for="item in menuItems" :key="item.title">
+                <v-list-group v-if="item.children" value="events">
+                    <template v-slot:activator="{ props }">
+                        <v-list-item v-bind="props" :title="item.title"></v-list-item>
+                    </template>
+                    <v-list-item
+                        v-for="child in item.children"
+                        :key="child.path"
+                        :title="child.title"
+                        :to="child.path"
+                        link
+                    ></v-list-item>
+                </v-list-group>
+                <v-list-item
+                    v-else
+                    :title="item.title"
+                    :to="item.path"
+                    link
+                ></v-list-item>
+            </template>
         </v-list>
     </v-navigation-drawer>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
+import { useRoute } from 'vue-router'
 import { useDisplay } from 'vuetify'
 
 const { mobile } = useDisplay()
+const route = useRoute()
 const drawer = ref(false)
+
+const eventsItems = [
+    { title: 'All events', path: '/events' },
+    { title: 'Italian Open 2026', path: '/events/italianopen2026' }
+]
+
+// The Events button only opens the menu, so it needs to light up by itself
+const eventsActive = computed(() => route.path.startsWith('/events'))
 
 const menuItems = [
     { title: 'Team', path: '/team' },
-    { title: 'Events', path: '/events' },
+    { title: 'Events', children: eventsItems },
     { title: 'Press', path: '/press' },
     { title: 'Media', path: '/media' },
     { title: 'Publications', path: '/publications' },
